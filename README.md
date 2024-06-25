54c7722c12a9d67f6285cde425ce0477fa1ce4a7
maisa <maaidamia@gmail.com>
2024-06-25 21:22:48 +0800

    update operation added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # employee-management-system-demo
-Employee Management System Demo
+Employee Management System Demo with CRUD operation
 
 -Spring Boot
 
--- src/main/java/com/example/employeedemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/employeedemo/service/EmployeeServiceImpl.java
@@ -1,8 +1,11 @@
 package com.example.employeedemo.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import com.example.employeedemo.model.Employee;
 import com.example.employeedemo.repository.EmployeeRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +23,17 @@ public class EmployeeServiceImpl implements EmployeeService {
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
     }
-    
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if(optional.isPresent()){
+            employee = optional.get();
+        }else{
+            throw new RuntimeException("Employee not found for id:" +id);
+        }
+        return employee;
+    }
+
 }
